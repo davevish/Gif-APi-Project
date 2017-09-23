c44d5fe82b24303b48c9ac9e567d345cf465b335
Vishvesh Dave <vishvesh01@yahoo.com>
2017-09-23 04:21:22 -0400

    more changes

---
+++ javascript.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+    var carCompanies = ["Ferrari", "McLaren", "Honda", "Lamborghini", "Porsche", "Pagani"];
+
+    function displayGifButtons() {
+        $("#buttons").empty();
+        for (var i = 0; i < actions.length; i++) {
+            var gifButton = $("<button>");
+            gifButton.addClass("action");
+            gifButton.addClass("btn btn-primary")
+            gifButton.attr("data-name", actions[i]);
+            gifButton.text(actions[i]);
+            $("#buttons").append(gifButton);
+        }
+    }
+
+    function makeNewButton() {
+        $("addNewButton").on("click", function() {
+            var carCompanies = ("input-area").val().trim();
+            carCompanies.push(carCompanies);
+
+            display.gifButton();
+        });
+    }
+
+    function showCarGifs() {
+        var carCompanies = $(this).attr("data-name");
+
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + cars + "&api_key=oxkAofrlO4kyulfi5hP4vBtGda2rUflL&limit=10";
+        $.ajax({
+            url: queryURL
+            method: get;
+        })
+    }
+
+    .done(function(response) {
+        $("#gifs").empty();
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+            var cargifdiv = $("<div>");
+            cargifdiv.addClass("cargifdiv");
+
+            var gifDisplay = $("<img>");
+            gifDisplay.attr("src", results[i].images.fixed_height_small_still.url);
+            gifDisplay.attr("data-still", results[i].images.fixed_height_small_still.url);
+            gifDisplay.attr("data-animate", results[i].images.fixed_height_small.url);
+            gifDisplay.attr("data-state", "still");
+            gifDisplay.addClass("image");
+            gifDiv.append(gifDisplay);
+            $("#gifs").prepend(gifDisplay);
+        }
+    })
+    displayGifButtons();
+    makeNewButton();
+    $(document).on("click", ".action", showCarGifs);
+    $(document).on("click", ".image", function() {
+
+        var state = $(this).attr('data-state');
+        if (state == 'still') {
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
+    });
+});
